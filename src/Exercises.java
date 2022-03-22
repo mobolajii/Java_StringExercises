47d450ed28b5980ec1edac238441d2ca1a613d7d
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-03-22 14:12:34 +0100

    Initial Commit

--- src/Exercises.java
+++ src/Exercises.java
@@ -3,32 +3,81 @@ import java.util.*;
 import java.lang.*;
 
 
-class Butterbrot{
+class StringExercises{
 
-    static boolean dop(String s){
+    static int[]checkIP(String s) {
+        StringTokenizer st = new StringTokenizer(s, ".");
+        ArrayList<String> elements = new ArrayList<String>();
+        int[]newD = new int[2];
 
-
-        for(int i=0; i<s.length(); i++){
-            int count = 0;
-
-            //System.out.println("count "+ count);
-
-           for (int j=0; j<s.length(); j++){
-               //System.out.println("s.charAt(i) "+ s.charAt(i));
-//               System.out.println("s.charAt(j) "+ s.charAt(j));
-               if(s.charAt(i) == s.charAt(j))count++;
-
-           }
-
-            if(count==2)return true;
+        for (int i = 0; i < st.countTokens(); i++) {
+            elements.add(st.nextToken());
         }
+        for (int i = 0; i < elements.size(); i++) {
+
+            String oneS = elements.get(0);
+            String twoS = elements.get(1);
+            //String threeS = elements.get(2);
+           // String fourS = elements.get(3);
+
+
+            int oneI = Integer.parseInt(oneS);
+            int twoI = Integer.parseInt(twoS);
+           // int threeI = Integer.parseInt(threeS);
+            //int fourI = Integer.parseInt(fourS);
+
+            if(oneI<0 && oneI>255)return null;
+            if(twoI<0 && twoI>255)return null;
+           // if(threeI<0 && threeI>255)return null;
+            //if(fourI<0 && fourI>255)return null;
+
+            newD = new int[]{oneI +'.', twoI+'.', /*threeI+'.'fourI*/ };
+
+
+        }
+
+
+
+            return newD;
+    }
+
+
+
+    static boolean isUniTrier(String s){
+
+        StringTokenizer st = new StringTokenizer(s, ".");
+        ArrayList<String> elements = new ArrayList<String>();
+
+        for(int i=0; i<st.countTokens(); i++){
+            elements.add(st.nextToken());
+        }
+        for(int i=0; i<elements.size(); i++){
+            String oneS = elements.get(0);
+            String twoS = elements.get(1);
+
+
+            int oneI = Integer.parseInt(oneS);
+            int twoI = Integer.parseInt(twoS);
+
+
+
+
+            if(oneI == 136 && twoI == 199)return true;
+
+
+
+        }
+
         return false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        System.out.println(dop(s));
+
+       System.out.println(checkIP(s));
+        System.out.println(isUniTrier(s));
     }
+
 }
 
