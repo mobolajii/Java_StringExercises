7f9826c561ec20a00f0719d58bab9eccdb5596a7
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-03-26 09:40:53 +0100

    Returns the Character that appears the most in the first String.

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,7 @@ class StringExercises{
         System.out.println("Change the cases into their opposite cases: " + Cases.cases(s));
         System.out.println("Is the first String a palindrome? " + Palindrome.palindrome(s));
         System.out.println("Does the given character appear twice in the first String? " + Twice.twice(s));
+        System.out.println("What character appears the most in the first String? " + Occurences.occurence(s));
 
         String[]info = Tokenize.tokenize(s);
         System.out.println("Return the tokenized String back below: ");
@@ -31,9 +32,6 @@ class StringExercises{
         System.out.println();
 
         System.out.println( Extract.extract(s));
-
-
-
 
 
     }
