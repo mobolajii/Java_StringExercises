59baa30a29726d0d176f8ab72ae2791c60306612
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-03-26 08:34:25 +0100

    Deletes all the consonants in the first String.

--- src/DeleteConsonants.java
+++ src/DeleteConsonants.java
@@ -1,8 +1,9 @@
 public class DeleteConsonants {
     //deletes all the consonants in a String
     static String deleteConsonants(String s){
-        String vowels = "[^aeiouAEIOU]";
-        return s.replaceAll(vowels, "");
-
+        String consonants = "[^aeiouAEIOU]";
+        return s.replaceAll(consonants, "");
     }
+
+
 }
