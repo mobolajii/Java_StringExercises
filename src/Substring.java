aca467e647a55bec77a01f559b6ac75ddb2494ec
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-03-26 09:24:59 +0100

    Returns how often a substring appears in the first String.

--- src/Substring.java
+++ src/Substring.java
@@ -1,16 +1,13 @@
 public class Substring {
-
     //counts how often a substring appears in a String
     static int substring(String s){
-
-        int i = 0;
         int count = 0;
+        int i= 0;
 
         while((i=s.indexOf(s.substring(0,4),i))!=-1){
             i++;
             count++;
         }
-
         return count;
     }
 }
