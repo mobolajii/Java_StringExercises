b890a50fd0e3bdbb379ba9ee7ef0fce77e0bbe7e
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-03-26 09:02:18 +0100

    Counts how often a reversed substring appears in the first String.

--- src/Reverse.java
+++ src/Reverse.java
@@ -3,15 +3,15 @@ public class Reverse {
     //count how often a reversed substring appears
     static int reverse(String s){
         StringBuilder sb = new StringBuilder(s.substring(0,4));
-        String rev = sb.reverse().toString();
-
-        int i=0;
+        String reverse = sb.reverse().toString();
         int count = 0;
+        int i=0;
 
-        while((s.indexOf(rev,i))!=-1){
+        while((i=s.indexOf(reverse, i))!=-1){
             i++;
             count++;
         }
+
         return count;
     }
 }
