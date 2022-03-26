e8f19b0a613bec26d97acd8983702de89b814d15
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-03-26 09:18:03 +0100

    Return a tokenized String array.

--- src/Tokenize.java
+++ src/Tokenize.java
@@ -1,16 +1,19 @@
 import java.util.StringTokenizer;
 
 public class Tokenize {
-    //return tokenized String
-    static String[]tokenize(String s){
-        StringTokenizer st = new StringTokenizer(s, " ");
-        String[]data = new String[st.countTokens()];
+        //return tokenized String
+        static String[] tokenize(String s){
+                String[]data = new String[s.length()];
+                StringTokenizer st = new StringTokenizer(s);
 
-        for(int i=0; i<data.length; i++){
-            data[i] = st.nextToken();
+                for(int i=0; i< st.countTokens(); i++){
+                        data[i] = st.nextToken();
+                }
+
+                return data;
         }
 
-        return data;
 
-    }
+
+
 }
