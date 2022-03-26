b06900b144aaf5c4ca2c28187965a6e1fec515ae
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-03-26 08:31:12 +0100

    Counts the number of vowels that are present in the first String.

--- src/CountVowels.java
+++ src/CountVowels.java
@@ -2,12 +2,16 @@ public class CountVowels {
     //counts the number of vowels in a String
     static int countVowels(String s){
         int count = 0;
+
         for(int i=0; i<s.length(); i++){
             char c = s.toLowerCase().charAt(i);
-            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+            if(c == 'a' || c=='e' || c=='o' || c=='i' || c=='u'){
                 count++;
             }
         }
         return count;
     }
+
+
+
 }
