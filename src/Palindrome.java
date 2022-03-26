d0de51c795c6539938ce1254395147f26dc7bc9f
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-03-26 08:54:48 +0100

    Returns true, if the first String is a palindrome.

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -2,9 +2,8 @@ public class Palindrome {
     //returns true if String is a palindrom
     static boolean palindrome(String s){
         StringBuilder sb = new StringBuilder(s);
-        String rev = sb.reverse().toString();
+        String reverse = sb.reverse().toString();
 
-        if(s.equals(rev))return true;
-        return false;
+        return s.equals(reverse);
     }
 }
