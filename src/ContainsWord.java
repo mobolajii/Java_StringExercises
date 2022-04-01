0025898b0989e489fea0870628c2c8d9ba7b6491
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-04-01 13:11:46 +0200

    Returns true if String contains a word.

--- src/ContainsWord.java
+++ src/ContainsWord.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class ContainsWord {
     //returns true if String contains a word
+
+
     static boolean containsWord(String s){
         System.out.println("Give in a word: ");
         Scanner sc = new Scanner(System.in);
@@ -9,4 +11,11 @@ public class ContainsWord {
 
         return s.contains(word);
     }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(containsWord(s));
+    }
+
 }
