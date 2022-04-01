6a8c16b64391f109697ba3a08004d7646c35aaf6
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-04-01 20:38:53 +0200

    Returns the longest word in the array.

--- src/LongestString.java
+++ src/LongestString.java
@@ -1,7 +1,24 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class LongestString {
     //returns the longest word
+    static String longestString(String s){
+        String length = " ";
+        String[]data = s.split(" ");
+        for(int i=0; i<data.length; i++){
+            if(data[i].length()>length.length()){
+                length = data[i];
+            }
+        }
+        return length;
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(longestString(s));
+    }
 
 }
