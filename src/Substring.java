25d1825467c48abf9257a76dfa7660184937f967
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-04-01 20:56:47 +0200

    Counts how often a substring appears in a String.

--- src/Substring.java
+++ src/Substring.java
@@ -1,14 +1,24 @@
+import java.util.Scanner;
+
 public class Substring {
     //counts how often a substring appears in a String
+
     static int substring(String s){
-        int i = 0;
         int count = 0;
+        int i = 0;
 
         while((i=s.indexOf(s.substring(0,4), i))!=-1){
-            i++;
             count++;
+            i++;
         }
-
         return count;
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(substring(s));
+    }
+
 }
