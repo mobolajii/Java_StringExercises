7b94b12131fea35870bc33c67a78b4cb1cbc1d4a
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-04-01 13:02:56 +0200

    Changes cases to their opposite cases.

--- src/Cases.java
+++ src/Cases.java
@@ -2,20 +2,28 @@ import java.util.Scanner;
 
 public class Cases {
     //changes cases to opposite cases
-    static String cases(String s){
-        StringBuilder sb = new StringBuilder();
-
-        for(int i=0; i<s.length(); i++){
+    static String cases(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if(Character.isLowerCase(c)){
-                Character.toUpperCase(c);
-            }else if(Character.isUpperCase(c)){
-                Character.toLowerCase(c);
+            if (Character.isLowerCase(c)) {
+                c = Character.toUpperCase(c);
+            } else if (Character.isUpperCase(c)) {
+                c = Character.toLowerCase(c);
             }
+
             sb.append(c);
         }
 
         return sb.toString();
-
     }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(cases(s));
+    }
+
 }
+
+
