cdbc6e4bfbeda53910a1df9b2615ba064782a4ef
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-04-01 13:54:29 +0200

    Deletes all the consonants in a String

--- src/DeleteConsonants.java
+++ src/DeleteConsonants.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class DeleteConsonants {
     //deletes all the consonants in a String
+
     static String deleteConsonants(String s){
         String consonants = "[^aeiouAEIOU]";
-
         return s.replaceAll(consonants, "");
     }
 
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(deleteConsonants(s));
+
+    }
 }
