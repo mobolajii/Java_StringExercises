5035e471be82cd30c50cc585c452eb1339a8bba8
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-04-01 20:45:57 +0200

    Returns true if String is a palindrome.

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class Palindrome {
     //returns true if String is a palindrome
+
     static boolean palindrome(String s){
         StringBuilder sb = new StringBuilder(s);
         String reverse = sb.reverse().toString();
 
-        if(s.equals(reverse))return true;
-        return false;
+        return s.equals(reverse);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(palindrome(s));
     }
 
 }
