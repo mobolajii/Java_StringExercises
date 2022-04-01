423b187b8f5142c81aeb1c6821b3fbfee1118661
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-04-01 20:22:10 +0200

    Returns true if two Strings are equal.

--- src/Equal.java
+++ src/Equal.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class Equal {
     //returns true if two Strings are equal
+    static boolean equal(String s){
+        System.out.println("Give in a word: ");
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
 
+        return s.equals(word);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(equal(s));
+    }
 }
