04be8e1b7c10a99a0be2caffb4a61f8cc235719b
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-04-01 20:32:08 +0200

    Extracts the word in the brackets.

--- src/Extract.java
+++ src/Extract.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Extract {
     //extracts the word in the brackets
+    static String extract(String s) {
+        String brackets = "\\[(.*?)\\]";
+        Pattern pattern = Pattern.compile(brackets);
+        Matcher matcher = pattern.matcher(s);
+
+        while (matcher.find()){
+            System.out.println(matcher.group(1));
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(extract(s));
+    }
 
 }
