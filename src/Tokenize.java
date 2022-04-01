ccccf866ca5cb294492e59bcf86a1c38fef9de9d
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-04-01 21:21:56 +0200

    Returns tokenized String.

--- src/Tokenize.java
+++ src/Tokenize.java
@@ -1,17 +1,34 @@
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Tokenize {
-        //return tokenized String
-    static String[]tokenize(String s){
+        //returns tokenized String
+
+    static String[] tokenize(String s){
         StringTokenizer st = new StringTokenizer(s, " ");
-        String[]data = new String[st.countTokens()];
+        String[]data = s.split(" ");
 
-        for(int i=0; i< data.length; i++){
-           data[i] = st.nextToken();
+        for(int i=0; i<data.length; i++){
+            data[i] = st.nextToken();
         }
-        return data;
 
+        return data;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        String[]data = tokenize(s);
+        for(int i=0; i<data.length; i++){
+            System.out.print (data[i] + " ");
+        }
 
     }
 
+
+
 }
+
+
