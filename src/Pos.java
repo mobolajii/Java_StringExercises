97403bb6bbade002ef498646375fef9fe1bb94d0
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-04-01 20:51:53 +0200

    Returns true if 'a' appears at two positions and is a character apart from each other.

--- src/Pos.java
+++ src/Pos.java
@@ -1,16 +1,25 @@
+import java.util.Scanner;
+
 public class Pos {
 
     //returns true if 'a' appears at two positions and is a character apart from each other
+
     static boolean pos(String s){
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i) == 'a'){
-                for(int j=i+1; j<s.length(); j++){
-                    if(s.charAt(j) =='a'){
-                        return true;
-                    }
+                for(int j= i+1; j<s.length(); j++ ){
+                    if(s.charAt(j) == 'a')return true;
+                    break;
                 }
             }
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(pos(s));
     }
 }
