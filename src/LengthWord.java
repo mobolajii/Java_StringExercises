6fe741bb99727a9d56a98dcf7b794a0a3b3e0ac3
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-04-01 20:31:06 +0200

    Counts how many words are in a StringArray.

--- src/LengthWord.java
+++ src/LengthWord.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class LengthWord {
-
     //counts how many words are in a StringArray
+    static int lengthWord(String s){
+        String[]data = s.split(" ");
 
+        return data.length;
+    }
+
+    static int lengthWords(String s){
+        StringTokenizer st = new StringTokenizer(s, " ");
+        String[]data = new String[st.countTokens()];
+
+       return data.length;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(lengthWord(s));
+        System.out.println(lengthWords(s));
+    }
 
 }
