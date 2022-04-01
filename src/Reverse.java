5e5e95d7c6043781ee6dafd27b8f90876a22500f
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-04-01 20:59:28 +0200

    Counts how often a reversed substring appears

--- src/Reverse.java
+++ src/Reverse.java
@@ -1,17 +1,27 @@
+import java.util.Scanner;
+
 public class Reverse {
 
     //count how often a reversed substring appears
     static int reverse(String s){
         int count = 0;
+        int i = 0;
+
         StringBuilder sb = new StringBuilder(s.substring(0,4));
         String reverse = sb.reverse().toString();
 
-       int i =0;
-       while((i=s.indexOf(reverse, i))!=-1){
-           i++;
-           count++;
-       }
-
-       return count;
+        while((i=s.indexOf(reverse, i))!=-1){
+            count++;
+            i++;
+        }
+        return count;
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(reverse(s));
+    }
+
 }
