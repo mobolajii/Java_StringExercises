159ca815e340316b15f5c04bfd030738658729f6
Moge1 <74511951+Moge1@users.noreply.github.com>
2022-04-01 21:34:25 +0200

    Returns true if character appears twice in a String

--- src/Twice.java
+++ src/Twice.java
@@ -6,11 +6,19 @@ public class Twice {
         System.out.println("Give in a character: ");
         Scanner sc = new Scanner(System.in);
         char c = sc.next().charAt(0);
-
+        int count = 0;
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == c)return true;
+            if(s.charAt(i) == c)count++;
         }
+
+        if(count ==2)return true;
         return false;
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(twice(s));
+    }
 }
